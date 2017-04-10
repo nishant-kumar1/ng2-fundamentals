b185d68310e10acca5bf19810f6a05aef8842e05
Nishant-Kumar1 <Nishant.Kumar17@cognizant.com>
2017-04-10 19:22:57 +0530

    Some files

---
+++ app/home/home.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core'
+
+@Component ({
+    selector : "home-component",
+    templateUrl : "/app/home/home.component.html"
+})
+
+export class HomeComponent{
+    
+}
---
+++ app/links/links.component.js
@@ -0,0 +1,1 @@
+//# sourceMappingURL=links.component.js.map
